0c1189e01f15053d9fb3689808f6e43ce59aea47
Deniken Lopes <denikeng@gmail.com>
2025-07-28 20:32:47 -0500

    Ambiente venv

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+windows = pygame.display.set_mode(size=(800, 480))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        
